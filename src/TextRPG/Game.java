51f60f035274b8312d97344a0fb041eac979a263
Jeung9711 <134358261+Jeung9711@users.noreply.github.com>
2025-04-12 00:08:28 +0900

    refactor: 2번째 피드백 반영, 메소드 리팩토링 및 프라이빗 변수선언

--- src/TextRPG/Game.java
+++ src/TextRPG/Game.java
@@ -3,36 +3,43 @@ package TextRPG;
 import java.util.Scanner;
 
 public class Game {
+    Player p = new Player();
+    Monster m = new Monster();
+    boolean result = true;
+    Scanner sc = new Scanner(System.in);
 
     // 실제 게임을 진행하는 메소드
     public void start() {
-        Player p = new Player();
-        p.hp = 30;
-        Monster m = new Monster();
-        m.hp = 50;
-        boolean result = true;
+        printStatus();
 
         while (result) {
-            System.out.printf("플레이어 체력: %s\n몬스터 체력: %s\n", p.hp, m.hp);
-
-            Scanner sc = new Scanner(System.in);
-            System.out.println("플레이어의 공격을 선택하세요\n 베기 / 찌르기");
-            String type = sc.next();
-            p.attack(m,type);
-            m.attack(p);
-
-            result = checkBattleResult(p,m);
+            playerTurn();
+            monsterTurn();
+            if(!checkBattleResult(p,m)) break;
         }
     }
 
+    public void printStatus() {
+        System.out.printf("플레이어 체력: %s\n몬스터 체력: %s\n", p.getHp(), m.getHp());
+    }
+
+    public void playerTurn() {
+        System.out.println("플레이어의 공격을 선택하세요\n 베기 / 찌르기");
+        String type = sc.next();
+        p.attack(m, type);
+    }
+    public void monsterTurn() {
+        m.attack(p);
+    }
+
     public boolean checkBattleResult(Player p, Monster m) {
-        if(p.hp <= 0) {
+        if(p.getHp() <= 0) {
             System.out.println("전투에서 패배하였습니다!");
             System.out.println("플레이어의 패배로 게임이 종료되었습니다.");
             return false;
-        } else if(m.hp <= 0) {
-            System.out.println("전투에서 승리하셨습니다!");
+        } else if(m.getHp() <= 0) {
             System.out.println("몬스터를 무찔렀습니다.");
+            System.out.println("전투에서 승리하셨습니다!");
             return false;
         } else {
             return true;
--- src/TextRPG/Monster.java
+++ src/TextRPG/Monster.java
@@ -3,27 +3,32 @@ package TextRPG;
 import java.util.Random;
 
 public class Monster {
-    int hp;
+    private int hp;
     Random random = new Random();
 
     public Monster() {}
-    public Monster(int hp) {
-        this.hp = hp;
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp){
+        if(hp>=0) {
+            this.hp = hp;
+        }
     }
 
     public void attack(Player p) {
-        int maxpower = (int) (p.hp /3);
-        if(maxpower < 1) maxpower=1;
-
-        int damage = random.nextInt(maxpower) +1;
+        int playerHp = p.getHp();
+        // max 함수 이용 난이도 조절
+        // 최솟값 1로 설정
+        int power = Math.max(1, (int) ((50 - playerHp) / 20));
+        int damage = random.nextInt(power) +1;
 
         p.takeDamage(damage);
     }
     
-    public int takeDamage(String type, int damage) {
+    public void takeDamage(String type, int damage) {
         this.hp -= damage;
         System.out.printf("몬스터가 플레이어의 %s 공격에 %s만큼의 공격을 입었다!\n몬스터 남은 체력: %s",
-                type,this.hp);
-        return this.hp;
+                type, damage, this.hp);
     }
 }
